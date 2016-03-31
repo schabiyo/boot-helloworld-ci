6ccf9ae1cf473dcede6e6ab3175076b84af4483c
Sani Chabi Yo <chabiyosani@gmail.com>
2016-03-31 15:55:27 -0400

    updates

--- ci/tasks/unit-test.sh
+++ ci/tasks/unit-test.sh
@@ -1,32 +1,5 @@
 #!/bin/sh
 
-inputDir=
-
-while [ $# -gt 0 ]; do
-  case $1 in
-    -i | --input-dir )
-      inputDir=$2
-      shift
-      ;;
-    * )
-      echo "Unrecognized option: $1" 1>&2
-      exit 1
-      ;;
-  esac
-  shift
-done
-
-error_and_exit() {
-  echo $1 >&2
-  exit 1
-}
-
-if [ ! -d "$inputDir" ]; then
-  error_and_exit "missing input directory: $inputDir"
-fi
-
-cd $inputDir
-
-echo $inputDir
+cd boot-helloworld
 
 ./mvnw clean test
