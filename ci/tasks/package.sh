e1b28e1ced96e5e35ee70538f4ff1c8f01f5e17f
Sani Chabi Yo <chabiyosani@gmail.com>
2016-03-30 23:07:09 -0400

    updating package script

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -1,11 +1,37 @@
 #!/bin/sh
 
+inputDir=  outputDir=  artifactId= 
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -i | --input-dir )
+      inputDir=$2
+      shift
+      ;;
+    -o | --output-dir )
+      outputDir=$2
+      shift
+      ;;
+    -a | --artifactId )
+      artifactId=$2
+      shift
+      ;;
+    * )
+      echo "Unrecognized option: $1" 1>&2
+      exit 1
+      ;;
+  esac
+  shift
+done
 
 
+artifactName="${artifactId}.jar"
 
 cd $inputDir
 ./mvnw clean package -Pci -DversionNumber=$version
 
 # Copy war file to concourse output folder
 cd ..
+
+cp $inputDir/manifest.yml package/manifest.yml
 cp $inputDir/target/$artifactName $outputDir/$artifactName
